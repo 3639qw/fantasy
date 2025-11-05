887925d9d1ef93234a87b41460772e79e9bec84e
안현웅 <woong92050@gmail.com>
2025-11-05 15:34:53 +0900

    풀 받고 moon으로 리베이스

---
+++ FantasyREPO/Assets/Script/Quest/Currency/WalletDebug.cs
@@ -0,0 +1,15 @@
+﻿// Assets/Scripts/Currency/WalletDebug.cs
+using UnityEditor;
+using UnityEngine;
+
+
+[DefaultExecutionOrder(-100)]
+public static class WalletDebug
+{
+    [MenuItem("Tools/Wallet/Delete Saved Gold")]
+    public static void DeleteSavedGold()
+    {
+        PlayerPrefs.DeleteKey("PLAYER_GOLD");
+        Debug.Log("[Wallet] Deleted PLAYER_GOLD key");
+    }
+}
