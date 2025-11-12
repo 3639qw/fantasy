ae870c4ad87238c96fc349864eb932ebe595f326
k-yoon03 <yoonnyang1013@gmail.com>
2025-11-12 11:35:56 +0900

    Revert "Merge remote-tracking branch 'origin/AHN' into Kyoon"
    This reverts commit 41d0474413d242c5d46429f2337dbd6996480a62, reversing
    changes made to d75acc40ee1f25c1e168addb09923a8dcf995f14.

--- FantasyREPO/Assets/Script/Quest/OneShotStartT003.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-public class OneShotStartT003 : MonoBehaviour
-{
-    public AngelCopperQuestT003 angel;
-    void Start()
-    {
-        if (angel) angel.StartQuest();
-    }
-}
